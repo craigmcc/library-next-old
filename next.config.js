0ed6afe297b0b0b88c1744419935b996580d31f7
Craig McClanahan <craigmcc@gmail.com>
2023-05-22 20:44:25 -0700

    Failed db init experiment based on https://github.com/vercel/next.js/discussions/15341#discussioncomment-2987699

--- next.config.js
+++ next.config.js
@@ -6,3 +6,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+/*
+module.exports = async (phase) => {
+    console.log("STARTING AT PHASE ", phase);
+    await import("./src/app/server/models/Database");
+    console.log("FINISHED DATABASE IMPORT");
+    return nextConfig;
+}
+*/
